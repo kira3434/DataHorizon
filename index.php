a7389437bd5c8a666f679e5393095d92bfaf35aa
Jhonatas Reis <83553776+kira3434@users.noreply.github.com>
2023-09-18 17:25:50 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DataHorizon</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div id="corpo">
+ <div class="braco-esquerdo">
+    <div class="caixa-cadastro">
+        <div class="titulo-cadastro">
+        </div>
+        <div class="cadastro">
+            <form action="index.php" method="get">
+                <input type="text" name="" placeholder= "Nome e Sobrenome " >
+                <input type="email" name="" placeholder= "E-mail">
+                <input type="password" name="" placeholder= "Senha">
+                <input type="password" name="" placeholder= "Confirmar senha ">
+            </form>
+        </div>
+    </div>
+ </div>
+ <div class="braco-direito">
+ <div class="caixa-titulo">
+    <div class="titulo"></div>
+ </div>
+ <div class="caixa-foto">
+    <div class="foto"></div>
+ </div>
+ </div>
+
+   </div>
+</body>
+</html>
